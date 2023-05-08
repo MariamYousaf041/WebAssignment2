3659f8947a8ccecb1fb8dc0048e5f2f024dc2442
MariamYousaf041 <maryummm041@gmail.com>
2023-05-08 20:39:24 +0500

    Add project files.

---
+++ WebAssignment2/Models/Database.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+namespace WebAssignment2.Models
+{
+    public class Database : DbContext
+    {
+        public Database(DbContextOptions<Database> options) : base(options)
+        {
+
+        }
+        public DbSet<Reviews> Reviews { get; set; }
+    }
+}
---
+++ WebAssignment2/Models/Reviews.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAssignment2.Models
+{
+    public class Reviews
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Product { get; set; }
+        public string Name { get; set; }
+        public string Feedback { get; set; }
+    }
+}
