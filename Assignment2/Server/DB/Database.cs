d31589b53044ad0b23ad12f27feebdd92faf330f
MariamYousaf041 <maryummm041@gmail.com>
2023-05-16 22:19:14 +0500

    change

---
+++ Assignment2/Server/DB/Database.cs
@@ -0,0 +1,14 @@
+﻿using Assignment2.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment2.Server.DB
+{
+    public class Database : DbContext
+    {
+        public Database(DbContextOptions<Database> options) : base(options)
+        {
+
+        }
+        public DbSet<Reviews> Reviews { get; set; }
+    }
+}
